a2d43ca23cf20afe6a3a2e9cdbba65f503cf4c9e
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-01-30 21:59:00 +0100

    create example.py

---
+++ example.py
@@ -0,0 +1,4 @@
+age = "20"
+print("Hello, my name is Hassan, I'm "+age+" years old, I like being "+age+" years old, because at "+age+" people are allowd to do a lot of things.") 
+print("With all my due respect")
+print("Hassan")
